473d3274e6f60d4f6dabe5fca0b7ea358f9a7f68
Mauricio Martínez Vargas <maurtz_bm@hotmail.com>
2020-04-15 00:20:12 -0500

    edit terminado. Falta search

--- src/app/product/component/list-product/list-product.component.ts
+++ src/app/product/component/list-product/list-product.component.ts
@@ -11,7 +11,16 @@ import { UpdateProductService } from '../../services/update-product.service';
   styleUrls: ['./list-product.component.css']
 })
 export class ListProductComponent implements OnInit {
-  products:Product = null;
+  products = null;
+  edit:boolean = false;
+
+  id=null;
+  nombre=null;
+  descripcion=null;
+  precio:number=null;
+
+  array=[];
+  array2=[];
 
   constructor(private listService:ListProductsService,
     private deleteP:DeleteProductService,
@@ -21,8 +30,13 @@ export class ListProductComponent implements OnInit {
     this.listProduct(); // el usuario debe verla desde el inicio, no debería llamarla. Debe ser automático.
   }
 
+  showEdit(id){
+    this.getId(id);
+    this.edit=true;    
+  }
+
   listProduct() {
-    this.listService.listProducts().subscribe( (result:Product) =>{
+    this.listService.listProducts().subscribe( (result) =>{
       this.products = result
       console.log(this.products);
       
@@ -30,11 +44,34 @@ export class ListProductComponent implements OnInit {
   }
 
   deleteProduct(idProduct) {
-    this.deleteP.deleteProduct(idProduct).subscribe();
+    this.deleteP.deleteProduct(idProduct).subscribe(
+    );
+  }
+
+  getId(id){
+   this.id=id;
+   console.log(this.id);
+  }
+
+
+  getArrJson(){
+    this.array=Array.of(this.products);
+    for(let arr of this.array[0]){
+      if(arr['id_producto']==this.id){
+        this.array2=arr;
+        this.array2['nombre_producto']=this.nombre;
+        this.array2['descripcion_producto']=this.descripcion;
+        this.array2['precio_producto']=this.precio;
+        
+      }
+    }
+    console.log(this.array2);
+    
+    return this.array2;
   }
 
   editProduct() {
-    this.editP.updateProduct(this.products).subscribe();
+    this.editP.updateProduct(this.getArrJson()).subscribe();
   }
 
 }
